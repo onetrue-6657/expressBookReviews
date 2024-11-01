8e1c34f148007776a4a2054dbd9f3b4e6b6779c4
onetrue-6657 <nz.osu@outlook.com>
2024-11-01 15:19:40 -0400

    npm install

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered"});
+        } else {
+            return res.status(404).json({message: "Unable to register. Username already exists."});
+        }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
