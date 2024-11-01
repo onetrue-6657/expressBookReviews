816b3c5dba12441e12596240005995b8e8049db0
onetrue-6657 <nz.osu@outlook.com>
2024-11-01 15:15:30 -0400

    auth

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { users } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,7 +12,14 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    let validUsers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    if (validUsers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 });
  
 const PORT =5000;
